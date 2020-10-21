1cddf1952684d9d8a48bd5a40a55f6baa7dca4a5
Christopher Henderson <chenderson5280@gmail.com>
2020-10-21 00:35:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon (file,emote)
         binding.pry
          return value[:japanese]
       else
-        return "Sorry, that emoticon was not found"
+         "Sorry, that emoticon was not found"
       end
 
     end
