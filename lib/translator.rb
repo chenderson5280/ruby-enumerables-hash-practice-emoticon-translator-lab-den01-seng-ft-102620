2d36b2af969b4fef511dc22a9fee374b59f85f98
Christopher Henderson <chenderson5280@gmail.com>
2020-10-21 00:30:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,15 +28,15 @@ def get_japanese_emoticon (file,emote)
 end
 
 
- result1 = YAML.load_file('lib/emoticons.yml')
- result1.each do |key, value|
-if value[0] == emote
-   return value[1]
-else
-   "Sorry, that emoticon was not found"
-end
-end
-end
+ #result1 = YAML.load_file('lib/emoticons.yml')
+ #result1.each do |key, value|
+#if value[0] == emote
+  # return value[1]
+#else
+  # "Sorry, that emoticon was not found"
+#end
+#end
+#end
 
 
 def get_english_meaning (result2,emote)
